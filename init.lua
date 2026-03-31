956f66788c3197b14d17052442c1b26e51a6da12
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-03-31 15:20:12 -0400

    Add bashls.lua and vimls.lua with config and add to lsp

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- Neovim v0.12.0 Configuration
 -- Resources:
 -- 	- https://echasnovski.com/blog/2026-03-13-a-guide-to-vim-pack
+-- 	- https://neovim.io/doc/user/lsp/
 
 -- Enable the built-in Lua bytecode cache (20-30% startup improvement).
 vim.loader.enable()
@@ -19,4 +20,16 @@ vim.keymap.set('i', '<CR>', function() return vim.fn.pumvisible() == 1 and '<C-y
 -- LSP
 -- Register mini.completion capabilities with all LSP servers
 vim.lsp.config('*', { capabilities = MiniCompletion.get_lsp_capabilities() })
-vim.lsp.enable({ 'lua_ls', 'rust_analyzer', 'gopls', 'pylsp', 'sqlls', 'dockerls', 'yamlls' })
+vim.lsp.enable({
+  'lua_ls',
+  'rust_analyzer',
+  'gopls',
+  'pylsp',
+  'sqlls',
+  'dockerls',
+  'yamlls',
+  'jsonls',
+  'taplo',
+  'bashls',
+  'vimls',
+})
