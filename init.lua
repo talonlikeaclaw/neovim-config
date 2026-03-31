0beee8dcd1460fdca687a0ba89c08d6b52e89d66
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-03-30 21:01:53 -0400

    Add init.lua with vim.loader.enable() directive

---
+++ init.lua
@@ -0,0 +1,6 @@
+-- Neovim v0.12.0 Configuration
+-- Resources:
+-- 	- https://echasnovski.com/blog/2026-03-13-a-guide-to-vim-pack
+
+-- Enable the built-in Lua bytecode cache (20-30% startup improvement).
+vim.loader.enable()
