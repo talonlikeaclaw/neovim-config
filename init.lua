bebc96e11f26ffeef055172c18bfd2ccca07357d
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-03-31 16:35:06 -0400

    Refactor config structure to use lua modules with bootstrap

--- init.lua
+++ init.lua
@@ -3,37 +3,12 @@
 -- 	- https://echasnovski.com/blog/2026-03-13-a-guide-to-vim-pack
 -- 	- https://neovim.io/doc/user/lsp/
 
--- Enable the built-in Lua bytecode cache (20-30% startup improvement).
 vim.loader.enable()
 
--- Colourscheme
-vim.pack.add({ { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } })
-vim.cmd.colorscheme("catppuccin-mocha")
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
--- Mini
-vim.pack.add({ { src = 'https://github.com/nvim-mini/mini.nvim', name = 'mini.nvim' } })
-
----- Completion
-require('mini.completion').setup()
-vim.keymap.set('i', '<CR>', function() return vim.fn.pumvisible() == 1 and '<C-y>' or '<CR>' end, { expr = true })
-
--- LSP
--- Register mini.completion capabilities with all LSP servers
-vim.lsp.config('*', { capabilities = MiniCompletion.get_lsp_capabilities() })
-vim.lsp.enable({
-  'lua_ls',
-  'rust_analyzer',
-  'gopls',
-  'pylsp',
-  'sqlls',
-  'dockerls',
-  'yamlls',
-  'jsonls',
-  'taplo',
-  'bashls',
-  'vimls',
-  'ts_ls',
-  'marksman',
-  'harper_ls',
-  'emmet_ls',
-})
+require('plugins')
+require('completion')
+require('clue')
+require('lsp')
---
+++ lua/plugins.lua
@@ -0,0 +1,6 @@
+vim.pack.add({
+  { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
+  { src = 'https://github.com/nvim-mini/mini.nvim', name = 'mini.nvim' },
+})
+
+vim.cmd.colorscheme('catppuccin-mocha')
