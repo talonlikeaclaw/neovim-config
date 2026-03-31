c244b14e99c54eac85a10b5f0b4f6fc555408aa0
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-03-31 17:06:00 -0400

    Add treesitter.lua plugin and install

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,11 @@
 vim.pack.add({
   { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
   { src = 'https://github.com/nvim-mini/mini.nvim', name = 'mini.nvim' },
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter', name = 'nvim-treesitter' },
 })
 
 vim.cmd.colorscheme('catppuccin-mocha')
 
 require('plugins.completion')
 require('plugins.clue')
+require('plugins.treesitter')
