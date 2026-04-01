61ade103c337e4bce480cae69f83693b739d62ca
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-03-31 23:11:14 -0400

    Add conform.nvim plugin with intiial formatting configuration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,11 +2,13 @@ vim.pack.add({
   { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
   { src = 'https://github.com/nvim-mini/mini.nvim', name = 'mini.nvim' },
   { src = 'https://github.com/nvim-treesitter/nvim-treesitter', name = 'nvim-treesitter' },
+  { src = 'https://github.com/stevearc/conform.nvim', name = 'conform.nvim' },
 })
 
 vim.cmd.colorscheme('catppuccin-mocha')
 
 require('plugins.notify')
+require('plugins.format')
 require('plugins.completion')
 require('plugins.clue')
 require('plugins.statusline')
